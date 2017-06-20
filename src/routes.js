68699a1dfe4f142927340d603fb59e739d8143b0
Casey Garland <casey@parsed.io>
2017-06-19 20:30:06 -0400

    add privacy route

--- src/routes.js
+++ src/routes.js
@@ -11,12 +11,17 @@ const goToBlog = (event) => {
     window.location = `http://blog.parsed.io${event.location.pathname}`;
 };
 
+const goToPrivacy = () => {
+    window.location = `https://drive.google.com/open?id=0B0KJCrmoxiokRnZEMEJ4SkU0Z1k`;
+};
+
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={HomePage}/>
     <Route path="/setup-a-mac-slave-for-jenkins/" onEnter={goToBlog}/>
     <Route path="/signup" component={Signup}/>
     <Route path="/front-end-roadmap" component={Roadmap}/>
+    <Route path="/privacy" onEnter={goToPrivacy}/>
     <Route path="*" component={HomePage}/>
   </Route>
 );
